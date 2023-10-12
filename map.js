f791aa2e162841e20c147fbb38153d5356858477
Moawiya <111635333+Moawiya-Sajjad@users.noreply.github.com>
2023-10-12 14:48:30 +0800

    Uploaded all files

---
+++ map.js
@@ -0,0 +1,11 @@
+var map_1 = "map.vg.json";
+var bubbleplot_1 = "bubbleplot.vg.json";
+var histogram_1 = "histogram.vg.json";
+var slopechart_1 = "slopechart.vg.json";
+var stackedbarchart_1 = "stackedbarchart.vg.json";
+
+vegaEmbed("#map", map_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#bubbleplot", bubbleplot_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#histogram", histogram_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#slopechart", slopechart_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#stackedbarchart", stackedbarchart_1).then(function (result) {}).catch(console.error);
